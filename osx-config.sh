27131c208bde5a8fd86154dcbd038306ae79a118
joelthecoder <joelthecoder@gmail.com>
2020-11-01 18:54:37 -0500

    Add default osx config

---
+++ osx-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Speed up keyboard repeat
+# https://apple.stackexchange.com/a/83923
+defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
