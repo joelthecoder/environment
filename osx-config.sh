2411100749d11bef6909a14bf7f092132b2616fa
joelthecoder <joelthecoder@gmail.com>
2020-11-17 13:16:03 -0500

    Call espanso setup script

--- osx-config.sh
+++ osx-config.sh
@@ -5,3 +5,5 @@
 defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
+# Run the Espanso setup
+./espanso/setup-espanso.sh
